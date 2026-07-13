c8ddfa17872c4f880fb1051477a70866e505ae05
Aminata2003 <sow.aminata17@ugb.edu.sn>
2026-07-13 14:01:44 +0000

    ajout du fichier pour le routage

--- japale/lib/choix_profil.dart
+++ japale/lib/choix_profil.dart
@@ -161,7 +161,7 @@ class ChoixProfilPage extends StatelessWidget {
                   ),
                 ],
               ),
-            ),
+            ),   
           ],
         ),
       ),
---
+++ japale/lib/routes.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import 'package:japale/choix_profil.dart'; // adapte le nom du fichier si besoin
+import 'package:japale/widgets/connexion.dart';
+import 'package:japale/inscription_etudiant.dart';
+import 'package:japale/inscription_livreur.dart';
+// import 'package:japale/inscription_restaurant.dart'; // à décommenter une fois créé
+import 'package:japale/acceuil_client.dart';
+//import 'package:japale/dashboard.dart';
+// import 'package:japale/table_de_bord_restaurant.dart';
+// import 'package:japale/menu_restaurant.dart';
+// import 'package:japale/mon_panier.dart';
+// import 'package:japale/suivi_commande.dart';
+// import 'package:japale/profil_etudiant.dart';
+// import 'package:japale/profil_livreur.dart';
+// import 'package:japale/gestion_de_menu.dart';
+// import 'package:japale/publication_menu_restaurant.dart';
+// import 'package:japale/alerte.dart';
+
+/// Toutes les routes de l'application sont déclarées ici.
+/// Pour ajouter une nouvelle page : ajoute une constante + une entrée dans
+/// `routes`. Tu n'as plus jamais besoin de toucher à main.dart.
+class AppRoutes {
+  static const String choixProfil = '/';
+  static const String connexion = '/connexion';
+  static const String inscriptionEtudiant = '/inscription-etudiant';
+  static const String inscriptionLivreur = '/inscription-livreur';
+  static const String inscriptionRestaurant = '/inscription-restaurant';
+  static const String accueilClient = '/accueil-client';
+  static const String dashboardLivreur = '/dashboard-livreur';
+  static const String tableDeBordRestaurant = '/table-de-bord-restaurant';
+  static const String menuRestaurant = '/menu-restaurant';
+  static const String monPanier = '/mon-panier';
+  static const String suiviCommande = '/suivi-commande';
+  static const String profilEtudiant = '/profil-etudiant';
+  static const String profilLivreur = '/profil-livreur';
+  static const String gestionDeMenu = '/gestion-de-menu';
+  static const String publicationMenu = '/publication-menu';
+
+  static Map<String, WidgetBuilder> get routes {
+    return {
+      choixProfil: (context) => const ChoixProfilPage(),
+      connexion: (context) => const ConnexionPage(),
+      inscriptionEtudiant: (context) => const InscriptionEtudiant(),
+      inscriptionLivreur: (context) => const InscriptionLivreur(),
+      // inscriptionRestaurant: (context) => const InscriptionRestaurant(),
+      accueilClient: (context) => const  AccueilClient(),
+      //dashboardLivreur: (context) => const Dashbord(),
+      // tableDeBordRestaurant: (context) => const TableDeBordRestaurant(),
+      // menuRestaurant: (context) => const MenuRestaurant(),
+      // monPanier: (context) => const MonPanier(),
+      // suiviCommande: (context) => const SuiviCommande(),
+      // profilEtudiant: (context) => const ProfilEtudiant(),
+      // profilLivreur: (context) => const ProfilLivreur(),
+      // gestionDeMenu: (context) => const GestionDeMenu(),
+      // publicationMenu: (context) => const PublicationMenuRestaurant(),
+    };
+  }
+}
