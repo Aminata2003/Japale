ba31587993a2c9c4a8138ec869f239f093bc9836
Aminata2003 <sow.aminata17@ugb.edu.sn>
2026-07-13 13:25:35 +0000

    Résolution du conflit sur main.dart

--- japale/lib/main.dart
+++ japale/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ConnexionPage(), // Page d'accueil de l'application
+      home: const WelcomePage(), // Page d'accueil de l'application
     );
   }
 }
